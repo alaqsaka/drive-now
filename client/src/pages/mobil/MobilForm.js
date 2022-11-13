2ab19d412a96435d559f8410f61597d730a24a87
alaqsaka <aqsaabidin15@gmail.com>
2022-11-13 21:34:41 +0700

    chore: edit mobil form page

--- client/src/pages/mobil/MobilForm.js
+++ client/src/pages/mobil/MobilForm.js
@@ -1,7 +1,14 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 function MobilForm() {
-	return <div>tambah mobil disini</div>;
+	return (
+		<div>
+			<Typography variant="h4" gutterBottom>
+				Tambah Mobil
+			</Typography>
+		</div>
+	);
 }
 
 export default MobilForm;
