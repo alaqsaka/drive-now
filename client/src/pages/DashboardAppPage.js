6c5a1bc5259bd9cd2d19528bc36a0436cb2c6426
alaqsaka <aqsaabidin15@gmail.com>
2022-11-29 23:48:22 +0700

    chore: edit dashboard page

--- client/src/pages/DashboardAppPage.js
+++ client/src/pages/DashboardAppPage.js
@@ -15,7 +15,7 @@ import {
 	AppTrafficBySite,
 	AppWidgetSummary,
 	AppCurrentSubject,
-	AppConversionRates
+	AppConversionRates,
 } from "../sections/@dashboard/app";
 
 // ----------------------------------------------------------------------
@@ -36,19 +36,24 @@ export default function DashboardAppPage() {
 
 				<Grid container spacing={3}>
 					<Grid item xs={12} sm={6} md={3}>
-						<AppWidgetSummary title="Weekly Sales" total={714000} icon="ant-design:android-filled" />
+						<AppWidgetSummary title="Jumlah Transaksi" total={100} icon="ant-design:android-filled" />
 					</Grid>
 
 					<Grid item xs={12} sm={6} md={3}>
-						<AppWidgetSummary title="New Users" total={1352831} color="info" icon="ant-design:apple-filled" />
+						<AppWidgetSummary title="Jumlah User" total={80} color="info" icon="ant-design:apple-filled" />
 					</Grid>
 
 					<Grid item xs={12} sm={6} md={3}>
-						<AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon="ant-design:windows-filled" />
+						<AppWidgetSummary title="Jumlah Mobil" total={10} color="warning" icon="ant-design:windows-filled" />
 					</Grid>
 
 					<Grid item xs={12} sm={6} md={3}>
-						<AppWidgetSummary title="Bug Reports" total={234} color="error" icon="ant-design:bug-filled" />
+						<AppWidgetSummary
+							title="Jumlah Keuntungan (IDR)"
+							total={50000000}
+							color="error"
+							icon="ant-design:bug-filled"
+						/>
 					</Grid>
 
 					<Grid item xs={12} md={6} lg={8}>
@@ -66,27 +71,27 @@ export default function DashboardAppPage() {
 								"08/01/2003",
 								"09/01/2003",
 								"10/01/2003",
-								"11/01/2003"
+								"11/01/2003",
 							]}
 							chartData={[
 								{
 									name: "Team A",
 									type: "column",
 									fill: "solid",
-									data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
+									data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
 								},
 								{
 									name: "Team B",
 									type: "area",
 									fill: "gradient",
-									data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
+									data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
 								},
 								{
 									name: "Team C",
 									type: "line",
 									fill: "solid",
-									data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
-								}
+									data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+								},
 							]}
 						/>
 					</Grid>
@@ -98,13 +103,13 @@ export default function DashboardAppPage() {
 								{ label: "America", value: 4344 },
 								{ label: "Asia", value: 5435 },
 								{ label: "Europe", value: 1443 },
-								{ label: "Africa", value: 4443 }
+								{ label: "Africa", value: 4443 },
 							]}
 							chartColors={[
 								theme.palette.primary.main,
 								theme.palette.info.main,
 								theme.palette.warning.main,
-								theme.palette.error.main
+								theme.palette.error.main,
 							]}
 						/>
 					</Grid>
@@ -123,7 +128,7 @@ export default function DashboardAppPage() {
 								{ label: "South Korea", value: 690 },
 								{ label: "Netherlands", value: 1100 },
 								{ label: "United States", value: 1200 },
-								{ label: "United Kingdom", value: 1380 }
+								{ label: "United Kingdom", value: 1380 },
 							]}
 						/>
 					</Grid>
@@ -135,7 +140,7 @@ export default function DashboardAppPage() {
 							chartData={[
 								{ name: "Series 1", data: [80, 50, 30, 40, 100, 20] },
 								{ name: "Series 2", data: [20, 30, 40, 80, 20, 80] },
-								{ name: "Series 3", data: [44, 76, 78, 13, 43, 10] }
+								{ name: "Series 3", data: [44, 76, 78, 13, 43, 10] },
 							]}
 							chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
 						/>
@@ -149,7 +154,7 @@ export default function DashboardAppPage() {
 								title: faker.name.jobTitle(),
 								description: faker.name.jobTitle(),
 								image: `/assets/images/covers/cover_${index + 1}.jpg`,
-								postedAt: faker.date.recent()
+								postedAt: faker.date.recent(),
 							}))}
 						/>
 					</Grid>
@@ -164,10 +169,10 @@ export default function DashboardAppPage() {
 									"12 Invoices have been paid",
 									"Order #37745 from September",
 									"New order placed #XF-2356",
-									"New order placed #XF-2346"
+									"New order placed #XF-2346",
 								][index],
 								type: `order${index + 1}`,
-								time: faker.date.past()
+								time: faker.date.past(),
 							}))}
 						/>
 					</Grid>
@@ -179,23 +184,23 @@ export default function DashboardAppPage() {
 								{
 									name: "FaceBook",
 									value: 323234,
-									icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />
+									icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
 								},
 								{
 									name: "Google",
 									value: 341212,
-									icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />
+									icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
 								},
 								{
 									name: "Linkedin",
 									value: 411213,
-									icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />
+									icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
 								},
 								{
 									name: "Twitter",
 									value: 443232,
-									icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />
-								}
+									icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
+								},
 							]}
 						/>
 					</Grid>
@@ -208,7 +213,7 @@ export default function DashboardAppPage() {
 								{ id: "2", label: "Add SCSS and JS files if required" },
 								{ id: "3", label: "Stakeholder Meeting" },
 								{ id: "4", label: "Scoping & Estimations" },
-								{ id: "5", label: "Sprint Showcase" }
+								{ id: "5", label: "Sprint Showcase" },
 							]}
 						/>
 					</Grid>
